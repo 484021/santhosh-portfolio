80ab2893849779fe9a4403e879bc71f93401af2f
484021 <bhoopalsanthosh@gmail.com>
2024-02-15 09:15:49 -0500

    feat: introduction

--- components/intro.tsx
+++ components/intro.tsx
@@ -32,6 +32,19 @@ export default function Intro() {
                 >👋</motion.span>
             </div>
         </div>
+        <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Santhosh.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        a background in <span className="font-bold">management and consulting. </span>I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
+
     </section>
-  )
+
+  );
 }
