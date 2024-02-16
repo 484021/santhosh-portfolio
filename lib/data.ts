33bf859d1b54c4207cd2beec2229d1d26754b1cd
484021 <bhoopalsanthosh@gmail.com>
2024-02-15 23:19:03 -0500

    feat: 1st project in

--- lib/data.ts
+++ lib/data.ts
@@ -68,21 +68,7 @@ export const projectsData = [
       "I worked as a full-stack developer for my first client. This is a e-commerce app where users can buy hand-made bead accesories.",
     tags: ["Javascript", "HTML", "CSS", "Node.js", "Express"],
     imageUrl: ltbImg,
-  },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-  },
+  }
 ] as const;
 
 export const skillsData = [
