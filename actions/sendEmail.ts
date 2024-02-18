cc2de1fa1112cafda04e5fc754f829f875603626
484021 <bhoopalsanthosh@gmail.com>
2024-02-18 03:34:39 -0500

    feat: emails can be sent but there is no error catching.

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.devs>",
+      from: "Contact Form <onboarding@resend.dev>",
       to: "bhoopalsanthosh@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
